34841a4f094ce1212749e2eaf18c4d4493df3fd2
ERuvinga <ruvingaelie@gmail.com>
2023-04-15 10:31:24 +0200

    ADDING millis() function for adding intervall time to read temperature

--- depanIncubSantos.ino
+++ depanIncubSantos.ino
@@ -4,6 +4,7 @@ int sensorPin;    // select the input pin for the potentiometer
 int RelaisPin;
 int sensorValue;
 float tempe;
+unsigned long read_time;
  
 
 void setup() {
@@ -15,21 +16,25 @@ void setup() {
   // defin type of pin
   pinMode(RelaisPin, OUTPUT);
   digitalWrite(RelaisPin, LOW);
+  read_time = millis();
 }
 
 void loop() {
-  delay(1000);
+  if((millis() - read_time) >= 4000){
+    read_time = millis();
+    
   // read the value from the sensor:
   sensorValue = analogRead(sensorPin);
   tempe = ((961 - sensorValue)/11.0)-1.3 ;
-  Serial.println(tempe);
+  Serial.println(tempe);  
+  }
 
   //Controller of Relais
   if(tempe >= 37.7){
     digitalWrite(RelaisPin, LOW);
   }
 
-   if (tempe <= 37.3){
+   if (tempe <= 37.4){
     digitalWrite(RelaisPin, HIGH);
   }
 }
